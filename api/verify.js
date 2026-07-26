110b60de916745f48bcc2aeec37e586ffab30e6f
yaassenahmed2021-del <yaassenahmed2021@gmail.com>
2026-07-26 10:00:45 +0300

    Update VALID_KEYS in verify.js

--- api/verify.js
+++ api/verify.js
@@ -4,7 +4,7 @@ import { kv } from '@vercel/kv';
 const APP_NAME = 'payroll_v25_r2';
 
 // قائمة المفاتيح المسموح بها فقط
-const VALID_KEYS = ["hdhdghdhfh"];
+const VALID_KEYS = ["ahmedsayed"];
 
 export default async function handler(req, res) {
     // إعدادات الـ CORS
