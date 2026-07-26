e60a0785f22e2f0bba0eac643160692c1ed94b28
yaassenahmed2021-del <yaassenahmed2021@gmail.com>
2026-07-26 08:39:58 +0300

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -4,7 +4,7 @@ import { kv } from '@vercel/kv';
 const APP_NAME = 'payroll_v25_r2';
 
 // قائمة المفاتيح المسموح بها فقط
-const VALID_KEYS = ["JK0-MJP-NO0980-2026-HR"];
+const VALID_KEYS = ["hdhdghdhfh"];
 
 export default async function handler(req, res) {
     // إعدادات الـ CORS
